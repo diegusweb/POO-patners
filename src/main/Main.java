90e5a69de466c32dc56c59ed0e204f3e6b8cae35
Diego Rueda <diego.rueda2@gmail.com>
2018-11-19 19:46:32 -0400

    principio de liskov en progreso

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,26 +7,10 @@ public class Main {
 
     public static void main(String[] args){
 
-        //instancia crear un nuevo objeto
-        Jaguar yagua = new Jaguar(20, 120f);
-        //yagua.setEdad(10);
-        //yagua.setPeso(110f);
-
-        System.out.println(yagua);
-        yagua.comer();
-        yagua.dormir();
+        //instancia crear un nuevo objeto con principio de liskov
+        IFelino yagua = new Jaguar(20, 120f);
 
         yagua.cazar();
-
-        yagua.rugir();
-
-        //yagua.maullar();
-
-        Jaguar yagua2 = new Jaguar();
-        System.out.println(yagua2);
-
-        Jaguar yagua3 = new Jaguar(11);
-        System.out.println(yagua3);
     }
 }
 
